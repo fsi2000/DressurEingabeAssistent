f3354d3068c2b6e6e550dfa2a71f7c0755fb2407
Felix S <fsimmet@gmx.net>
2020-05-25 11:58:26 +0200

    Initial commit

---
+++ DressurEingabeAssistent/frmDEAMain.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDEAMain
+	Inherits System.Windows.Forms.Form
+
+	'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Wird vom Windows Form-Designer benötigt.
+	Private components As System.ComponentModel.IContainer
+
+	'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+	'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+	'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Me.cmdAddScore = New System.Windows.Forms.Button()
+		Me.lstScores = New System.Windows.Forms.ListBox()
+		Me.txtScore = New System.Windows.Forms.TextBox()
+		Me.cmdSendScore = New System.Windows.Forms.Button()
+		Me.cmdClearScore = New System.Windows.Forms.Button()
+		Me.cmdModifyScore = New System.Windows.Forms.Button()
+		Me.cmdDeleteScore = New System.Windows.Forms.Button()
+		Me.tmrSendScores = New System.Windows.Forms.Timer(Me.components)
+		Me.chkSpeech = New System.Windows.Forms.CheckBox()
+		Me.chkRestrictFullHalf = New System.Windows.Forms.CheckBox()
+		Me.SuspendLayout()
+		'
+		'cmdAddScore
+		'
+		Me.cmdAddScore.Location = New System.Drawing.Point(84, 4)
+		Me.cmdAddScore.Name = "cmdAddScore"
+		Me.cmdAddScore.Size = New System.Drawing.Size(36, 23)
+		Me.cmdAddScore.TabIndex = 1
+		Me.cmdAddScore.Text = "add"
+		Me.cmdAddScore.UseVisualStyleBackColor = True
+		'
+		'lstScores
+		'
+		Me.lstScores.FormattingEnabled = True
+		Me.lstScores.IntegralHeight = False
+		Me.lstScores.Location = New System.Drawing.Point(8, 32)
+		Me.lstScores.MultiColumn = True
+		Me.lstScores.Name = "lstScores"
+		Me.lstScores.Size = New System.Drawing.Size(184, 300)
+		Me.lstScores.TabIndex = 3
+		'
+		'txtScore
+		'
+		Me.txtScore.Location = New System.Drawing.Point(8, 4)
+		Me.txtScore.MaxLength = 3
+		Me.txtScore.Name = "txtScore"
+		Me.txtScore.Size = New System.Drawing.Size(60, 20)
+		Me.txtScore.TabIndex = 0
+		'
+		'cmdSendScore
+		'
+		Me.cmdSendScore.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+		Me.cmdSendScore.Location = New System.Drawing.Point(8, 336)
+		Me.cmdSendScore.Name = "cmdSendScore"
+		Me.cmdSendScore.Size = New System.Drawing.Size(76, 23)
+		Me.cmdSendScore.TabIndex = 4
+		Me.cmdSendScore.Text = "send"
+		Me.cmdSendScore.UseVisualStyleBackColor = True
+		'
+		'cmdClearScore
+		'
+		Me.cmdClearScore.Location = New System.Drawing.Point(116, 336)
+		Me.cmdClearScore.Name = "cmdClearScore"
+		Me.cmdClearScore.Size = New System.Drawing.Size(76, 23)
+		Me.cmdClearScore.TabIndex = 5
+		Me.cmdClearScore.Text = "clear"
+		Me.cmdClearScore.UseVisualStyleBackColor = True
+		'
+		'cmdModifyScore
+		'
+		Me.cmdModifyScore.Location = New System.Drawing.Point(120, 4)
+		Me.cmdModifyScore.Name = "cmdModifyScore"
+		Me.cmdModifyScore.Size = New System.Drawing.Size(36, 23)
+		Me.cmdModifyScore.TabIndex = 6
+		Me.cmdModifyScore.Text = "mod"
+		Me.cmdModifyScore.UseVisualStyleBackColor = True
+		'
+		'cmdDeleteScore
+		'
+		Me.cmdDeleteScore.Location = New System.Drawing.Point(156, 4)
+		Me.cmdDeleteScore.Name = "cmdDeleteScore"
+		Me.cmdDeleteScore.Size = New System.Drawing.Size(36, 23)
+		Me.cmdDeleteScore.TabIndex = 7
+		Me.cmdDeleteScore.Text = "del"
+		Me.cmdDeleteScore.UseVisualStyleBackColor = True
+		'
+		'tmrSendScores
+		'
+		Me.tmrSendScores.Interval = 1000
+		'
+		'chkSpeech
+		'
+		Me.chkSpeech.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+		Me.chkSpeech.Checked = True
+		Me.chkSpeech.CheckState = System.Windows.Forms.CheckState.Checked
+		Me.chkSpeech.Location = New System.Drawing.Point(8, 364)
+		Me.chkSpeech.Name = "chkSpeech"
+		Me.chkSpeech.Size = New System.Drawing.Size(72, 20)
+		Me.chkSpeech.TabIndex = 8
+		Me.chkSpeech.Text = "Audio"
+		Me.chkSpeech.UseVisualStyleBackColor = True
+		'
+		'chkRestrictFullHalf
+		'
+		Me.chkRestrictFullHalf.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+		Me.chkRestrictFullHalf.Checked = True
+		Me.chkRestrictFullHalf.CheckState = System.Windows.Forms.CheckState.Checked
+		Me.chkRestrictFullHalf.Location = New System.Drawing.Point(96, 364)
+		Me.chkRestrictFullHalf.Name = "chkRestrictFullHalf"
+		Me.chkRestrictFullHalf.Size = New System.Drawing.Size(96, 20)
+		Me.chkRestrictFullHalf.TabIndex = 9
+		Me.chkRestrictFullHalf.Text = ",0 && ,5 only"
+		Me.chkRestrictFullHalf.UseVisualStyleBackColor = True
+		'
+		'frmDEAMain
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(198, 388)
+		Me.Controls.Add(Me.chkRestrictFullHalf)
+		Me.Controls.Add(Me.chkSpeech)
+		Me.Controls.Add(Me.cmdDeleteScore)
+		Me.Controls.Add(Me.cmdModifyScore)
+		Me.Controls.Add(Me.cmdClearScore)
+		Me.Controls.Add(Me.cmdSendScore)
+		Me.Controls.Add(Me.txtScore)
+		Me.Controls.Add(Me.lstScores)
+		Me.Controls.Add(Me.cmdAddScore)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+		Me.Name = "frmDEAMain"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "EingabeAssistent v0.3 (171225)"
+		Me.TopMost = True
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+	Friend WithEvents cmdAddScore As System.Windows.Forms.Button
+	Friend WithEvents lstScores As System.Windows.Forms.ListBox
+	Friend WithEvents txtScore As System.Windows.Forms.TextBox
+	Friend WithEvents cmdSendScore As System.Windows.Forms.Button
+	Friend WithEvents cmdClearScore As System.Windows.Forms.Button
+	Friend WithEvents cmdModifyScore As System.Windows.Forms.Button
+	Friend WithEvents cmdDeleteScore As System.Windows.Forms.Button
+	Friend WithEvents tmrSendScores As System.Windows.Forms.Timer
+	Friend WithEvents chkSpeech As CheckBox
+	Friend WithEvents chkRestrictFullHalf As CheckBox
+End Class
---
+++ DressurEingabeAssistent/frmDEAMain.vb
@@ -0,0 +1,193 @@
+﻿Public Class frmDEAMain
+	Dim sendscorestate As Integer
+
+	Private Function GetScoreText(ByVal count As Integer, ByVal score As String) As String
+		Return count.ToString + ": " + score
+	End Function
+
+	Private Function GetScore(ByVal s As String) As String
+		Dim i As Integer
+		i = s.IndexOf(":")
+		Return s.Substring(i + 2, s.Length - i - 2)
+	End Function
+
+	Private Sub cmdAddScore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAddScore.Click
+		Dim i As Integer
+		Dim s As String
+		Dim valid As Boolean
+		Dim value As Single
+		Dim s2 As String
+
+		i = lstScores.Items.Count
+		s = txtScore.Text
+
+		If s.Length = 2 Then
+			s = s.Insert(1, ",")
+		End If
+
+		valid = True
+
+		If chkRestrictFullHalf.Checked Then
+			s2 = s.Replace(",", ".")
+			value = Single.Parse(s2)
+			value = value - Math.Truncate(value)
+			If value > 0 And value < 0.5 Then
+				valid = False
+			End If
+			If value > 0.5 Then
+				valid = False
+			End If
+		End If
+
+		If s = "" Then
+			valid = False
+		End If
+
+		If valid Then
+			lstScores.Items.Add(GetScoreText(i + 1, s))
+			My.Computer.Audio.Play(My.Resources.beep, AudioPlayMode.Background)
+		Else
+			My.Computer.Audio.Play(My.Resources.beep_low, AudioPlayMode.Background)
+		End If
+
+		txtScore.Focus()
+		txtScore.SelectAll()
+	End Sub
+
+	Private Sub txtScore_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtScore.KeyDown
+		If e.KeyCode = Keys.Enter Then
+			e.Handled = True
+			e.SuppressKeyPress = True
+			cmdAddScore_Click(sender, e)
+		End If
+
+		If chkSpeech.Checked Then
+			If e.KeyCode = Keys.D0 Or e.KeyCode = Keys.NumPad0 Then
+				My.Computer.Audio.Play(My.Resources.null, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D1 Or e.KeyCode = Keys.NumPad1 Then
+				My.Computer.Audio.Play(My.Resources.eins, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D2 Or e.KeyCode = Keys.NumPad2 Then
+				My.Computer.Audio.Play(My.Resources.zwei, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D3 Or e.KeyCode = Keys.NumPad3 Then
+				My.Computer.Audio.Play(My.Resources.drei, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D4 Or e.KeyCode = Keys.NumPad4 Then
+				My.Computer.Audio.Play(My.Resources.vier, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D5 Or e.KeyCode = Keys.NumPad5 Then
+				My.Computer.Audio.Play(My.Resources.fuenf, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D6 Or e.KeyCode = Keys.NumPad6 Then
+				My.Computer.Audio.Play(My.Resources.sechs, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D7 Or e.KeyCode = Keys.NumPad7 Then
+				My.Computer.Audio.Play(My.Resources.sieben, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D8 Or e.KeyCode = Keys.NumPad8 Then
+				My.Computer.Audio.Play(My.Resources.acht, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.D9 Or e.KeyCode = Keys.NumPad9 Then
+				My.Computer.Audio.Play(My.Resources.neun, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.Oemcomma Or e.KeyCode = Keys.Decimal Then
+				My.Computer.Audio.Play(My.Resources.komma, AudioPlayMode.Background)
+			End If
+			If e.KeyCode = Keys.Back Then
+				My.Computer.Audio.Play(My.Resources.beep_low, AudioPlayMode.Background)
+			End If
+		End If
+	End Sub
+
+	Private Sub lstScores_DoubleClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstScores.DoubleClick
+		Dim s As String
+		s = lstScores.Items(lstScores.SelectedIndex)
+
+		txtScore.Text = GetScore(s)
+
+		txtScore.Focus()
+		txtScore.SelectAll()
+	End Sub
+
+	Private Sub cmdModifyScore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdModifyScore.Click
+		Dim i As Integer
+		Dim s As String
+
+		i = lstScores.SelectedIndex
+		s = txtScore.Text
+
+		If s.Length = 2 Then
+			s = s.Insert(1, ",")
+		End If
+
+		lstScores.Items(i) = GetScoreText(i + 1, s)
+
+		txtScore.Focus()
+		txtScore.SelectAll()
+	End Sub
+
+	Private Sub cmdDeleteScore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDeleteScore.Click
+		Dim i As Integer
+
+		i = lstScores.SelectedIndex
+		If i <> -1 Then
+			If MessageBox.Show("Eintrag löschen?", "Bestätigung", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+				lstScores.Items.RemoveAt(i)
+			End If
+		End If
+	End Sub
+
+	Private Sub cmdClearScore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdClearScore.Click
+		If MessageBox.Show("Alles löschen?", "Bestätigung", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+			lstScores.Items.Clear()
+		End If
+	End Sub
+
+	Private Sub cmdSendScore_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSendScore.Click
+		sendscorestate = 1
+		tmrSendScores.Start()
+	End Sub
+
+	Private Sub AutomationSwitchToTORIS()
+		SendKeys.Send("%{Tab}")
+	End Sub
+
+	Private Sub AutomationSendValuesToTORIS()
+		Dim i As Integer
+		Dim j As Integer
+		Dim s As String
+
+		For i = 0 To lstScores.Items.Count - 1
+			lstScores.SelectedIndex = i
+			s = lstScores.Items(i)
+			s = GetScore(s)
+			SendKeys.Send(s)
+			SendKeys.Send("{ENTER}")
+			For j = 0 To 30000 : Application.DoEvents() : Application.DoEvents() : Application.DoEvents() : Next
+		Next
+	End Sub
+
+	Private Sub AutomationSwitchBackToDEA()
+		SendKeys.Send("%{Tab}")
+	End Sub
+
+	Private Sub tmrSendScores_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrSendScores.Tick
+		tmrSendScores.Stop()
+
+		Select Case sendscorestate
+			Case 1
+				AutomationSwitchToTORIS()
+			Case 2
+				AutomationSendValuesToTORIS()
+			Case 3
+				AutomationSwitchBackToDEA()
+		End Select
+
+		sendscorestate = sendscorestate + 1
+		If sendscorestate > 100 Then sendscorestate = 100
+
+		tmrSendScores.Start()
+	End Sub
+End Class
